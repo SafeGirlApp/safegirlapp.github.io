9f244b0f4962061df7fd82cdfb5ff966115f22fb
Mateus Bezerra <mateusbzerra@gmail.com>
2019-04-24 23:01:56 -0300

    hashHistory added

---
+++ precache-manifest.7fc0e5b4b1e7e0e6c21c52ff1854a070.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7785854c5742654b6aa29b90c4b2fb27",
+    "url": "/static/media/welcome.7785854c.png"
+  },
+  {
+    "revision": "fe33c1a1b11e4b2c468e4a6a52d3ef79",
+    "url": "/static/media/map.fe33c1a1.png"
+  },
+  {
+    "revision": "9e27453e54c1a08292285a3b47b9b1d8",
+    "url": "/static/media/main2.9e27453e.png"
+  },
+  {
+    "revision": "b37f5a1ae5a54beb4b0d3b2981f76ea4",
+    "url": "/static/media/login.b37f5a1a.png"
+  },
+  {
+    "revision": "86733bc4a06897c5f11f8abe90b8acc4",
+    "url": "/static/media/iconnew.86733bc4.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5023dd82fbe8f5b8dbfc",
+    "url": "/static/js/main.f75dbce6.chunk.js"
+  },
+  {
+    "revision": "50b6b11dfae3d3f91c29",
+    "url": "/static/js/2.5a754214.chunk.js"
+  },
+  {
+    "revision": "5023dd82fbe8f5b8dbfc",
+    "url": "/static/css/main.17b05f6d.chunk.css"
+  },
+  {
+    "revision": "51ca6fd543ef90c89832c9303e6ca233",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.338ec4627238f2866e73c7f027050176.js"
+  "/precache-manifest.7fc0e5b4b1e7e0e6c21c52ff1854a070.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
